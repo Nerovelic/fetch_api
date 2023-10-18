8736538fca2c7bdf2ffca8374bd88ec3abb01db1
Nerovelic <zack.vadhir2@hotmail.com>
2023-10-17 21:56:48 -0700

    ahora hay una galeria en los productos

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -12,12 +12,16 @@ async function obtenerProducto(id) {
 const Page = ({ params: { id } }) => {
   const router = useRouter();
   const [data, setData] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
     const obtenerDatosProducto = async () => {
       try {
         const productData = await obtenerProducto(id);
         setData(productData);
+        if (productData.images && productData.images.length > 0) {
+          setSelectedImage(productData.images[0]);
+        }
       } catch (error) {
         console.error("Error al obtener los datos del producto:", error);
       }
@@ -28,6 +32,10 @@ const Page = ({ params: { id } }) => {
 
   const redirectToHome = () => {
     router.push("/");
+  };
+
+  const handleThumbnailClick = (image) => {
+    setSelectedImage(image);
   };
 
   return (
@@ -46,14 +54,51 @@ const Page = ({ params: { id } }) => {
         )}
       </div>
       <h2 className="text-center py-5">
-        {data ? data.description : "Cargando..."}
+        Eslogan: {data ? data.description : "Cargando..."}
       </h2>
+      <h2 className="text-center py-5">Marca: {data?.brand}</h2>
+      <h2 className="text-center py-5">Categoria: {data?.category}</h2>
+
+      <h1 className="text-center text-2xl py-2"> Galeria de Fotos</h1>
+      <div className="flex justify-center pb-5">
+        {selectedImage && (
+          <Image
+            className="py-5"
+            src={selectedImage}
+            alt="product image"
+            width={300}
+            height={300}
+            style={{ width: 300, height: 300 }}
+          />
+        )}
+      </div>
+      <div className="flex justify-center">
+        {data && data.images && data.images.length > 0 && (
+          <div className="flex justify-center">
+            {data.images.map((image, index) => (
+              <div
+                key={index}
+                style={{ marginRight: "10px", cursor: "pointer" }}
+                onClick={() => handleThumbnailClick(image)}
+              >
+                <Image
+                  src={image}
+                  alt={`product image ${index}`}
+                  width={100}
+                  height={100}
+                />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
       <button
         className="p-2 bg-red-400 rounded-md px-5"
         onClick={redirectToHome}
         style={{
           position: "absolute",
-          top: "190%",
+          bottom: "10px",
           left: "10px",
         }}
       >
